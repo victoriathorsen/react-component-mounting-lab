9df39b0a47f6328f4d256948f14ab54303472856
Victoria <victoriathorsenn@gmail.com>
2021-07-06 15:57:48 -0400

    commit tree a5d665aeee9743a3db989fbfe88c0ef291fae76a

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import React, { Component } from 'react';
 import Timer from './Timer'
 
 class App extends Component {
-
   //no props being used here, so we can use the shorthand declaration of state
   state = {
     timerIDs: []
   }
 
 
-  //Your code here:
+  componentDidMount(){
+    this.handleAddTimer()
+  }
 
 
 
